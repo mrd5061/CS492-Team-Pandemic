c1676d9bd2b483f274b86760a7e8cb5fd4895109
Meghan Dougherty <35378202+mrd5061@users.noreply.github.com>
2020-04-29 21:16:11 -0700

    tweaked SQL queries
    Updated queries to correctly reference the test schema tables

--- database-files/pandemic-queries.sql
+++ database-files/pandemic-queries.sql
@@ -1,48 +1,50 @@
+#The following queries pull data from the test database schema.
+
 
 #Select the total beds and beds per 1000 people from a specific state
 #Replace Wyoming with desired state
-SELECT total_beds, beds_per_1k, b.state_name 
-FROM "HOSPITAL_BEDS" as a
-	INNER JOIN "STATE_OR_PROVINCE" as b on a.state_id = b.state_id
-WHERE state_name = 'Wyoming';
+SELECT a."TOTAL_BEDS", a."BEDS_PER_1K", b."STATE_NAME" 
+FROM test."HOSPITAL_BEDS" as a
+	INNER JOIN test."STATE_OR_PROVINCE" as b on a."STATE_ID" = b."STATE_ID"
+WHERE "STATE_NAME" = 'Wyoming';
 
 #Like above, but just the total beds
-SELECT total_beds, b.state_name 
-FROM "HOSPITAL_BEDS" as a
-	INNER JOIN "STATE_OR_PROVINCE" as b on a.state_id = b.state_id
-WHERE state_name = 'Wyoming';
+SELECT "TOTAL_BEDS", b."STATE_NAME" 
+FROM test."HOSPITAL_BEDS" as a
+	INNER JOIN test."STATE_OR_PROVINCE" as b on a."STATE_ID" = b."STATE_ID"
+WHERE "STATE_NAME" = 'Wyoming';
 
 #Like above, but just the beds per 1k
-SELECT beds_per_1k, b.state_name 
-FROM "HOSPITAL_BEDS" as a
-	INNER JOIN "STATE_OR_PROVINCE" as b on a.state_id = b.state_id
-WHERE state_name = 'Wyoming';
+SELECT "BEDS_PER_1K", b."STATE_NAME" 
+FROM test."HOSPITAL_BEDS" as a
+	INNER JOIN test."STATE_OR_PROVINCE" as b on a."STATE_ID" = b."STATE_ID"
+WHERE "STATE_NAME" = 'Wyoming';
 
 #Update the total beds in hospital beds for a desired state
-UPDATE "HOSPITAL_BEDS"
-SET total_beds = 2015
-	FROM"STATE_OR_PROVINCE" 
-WHERE state_name = 'Wyoming';
+UPDATE test."HOSPITAL_BEDS"
+SET "TOTAL_BEDS" = 2015
+	FROM test."STATE_OR_PROVINCE" 
+WHERE "STATE_NAME" = 'Wyoming';
 
 #same as above, but for beds per 1k
-UPDATE "HOSPITAL_BEDS"
-SET beds_per_1k = 2015
-	FROM"STATE_OR_PROVINCE" 
-WHERE state_name = 'Wyoming';
+UPDATE test."HOSPITAL_BEDS"
+SET "BEDS_PER_1K" = 9.2
+	FROM test."STATE_OR_PROVINCE" 
+WHERE "STATE_NAME" = 'Wyoming';
 
 #Returns the Latitude and Longitude of a specified state name
-SELECT latitude, longitude
-FROM "STATE_OR_PROVINCE"	
-WHERE state_name = 'Wyoming';
+SELECT "LATITUDE", "LONGITUDE"
+FROM test."STATE_OR_PROVINCE"	
+WHERE "STATE_NAME" = 'Wyoming';
 
 #Same as above but for a specified FIPS code.
-SELECT latitude, longitude
-FROM "STATE_OR_PROVINCE"	
+SELECT "LATITUDE", "LONGITUDE"
+FROM test."STATE_OR_PROVINCE"	
 WHERE "FIPS" = 56;
 
 #This is applicable if we end up including more geographic regions. 
 #Returns the country name for a specified state or province.
-SELECT country_name
-FROM "COUNTRIES" as a
-	INNER JOIN "STATE_OR_PROVINCE" as b on a.country_id = b.country_id
-WHERE a.state_name = 'Wyoming';
+SELECT "COUNTRY_NAME"
+FROM test."COUNTRIES" as a
+	INNER JOIN test."STATE_OR_PROVINCE" as b on a."COUNTRY_ID" = b."COUNTRY_ID"
+WHERE b."STATE_NAME" = 'Wyoming';
