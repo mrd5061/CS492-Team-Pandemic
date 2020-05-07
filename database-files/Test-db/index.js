626af9a7bb68f297437b5292a46f7d7bce194805
Meghan Dougherty <35378202+mrd5061@users.noreply.github.com>
2020-05-06 18:54:00 -0700

    adding comments to Test-db/index.js

--- database-files/Test-db/index.js
+++ database-files/Test-db/index.js
@@ -1,3 +1,9 @@
+//A proof of concept that the database can be connected to and accessed from a node.js server set up. 
+//requires installing https://node-postgres.com/ 
+//Built in reference to this tutorial: https://blog.logrocket.com/setting-up-a-restful-api-with-node-js-and-postgresql-d96d6fc892d8/
+//getData spits out a JSON object full of all the contents of the "REPORTED_DEATHS" table in the pandemicdb database. 
+
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
